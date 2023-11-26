e137b477bc196879f227650647b55583b58bba1e
Nariman <mirmanov1@mail.ru>
2023-11-26 15:02:00 +0600

    add my project

---
+++ src/View/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Main from './pages/Main';
+
+import './App.css';
+const App = () => {
+  return (
+    <div>
+      <h1>Shop</h1>
+      <Main />
+    </div>
+  );
+};
+
+export default App;
