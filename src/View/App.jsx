caad130e2abfc2702c120f0cfb97c166af0c5296
Nariman <mirmanov1@mail.ru>
2023-11-27 10:32:40 +0600

    add view, data, business

--- src/View/App.jsx
+++ src/View/App.jsx
@@ -5,9 +5,10 @@ import Main from './pages/Main';
 import './App.css';
 const App = () => {
   return (
-    <div>
-      <h1>Shop</h1>
-      <Main />
+    <div className='AppPage'>
+      <div className='AppPage_wrapper'>
+        <Main />
+      </div>
     </div>
   );
 };
---
+++ src/View/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='headerComponent'>
+      <h1>Explore Shop</h1>
+    </div>
+  );
+};
+
+export default Header;
